198ba069df337d622601404b28854a8c7a466862
cathgiese <125225131+cathgiese@users.noreply.github.com>
2023-12-20 23:11:02 -0800

    deploy

---
+++ build/precache-manifest.26fd7758fc6d3a064fbacfb87b00a657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a8962b00a385545f8dc04632545f86",
+    "url": "/assignment-3-cathgiese/index.html"
+  },
+  {
+    "revision": "1bfcb6979b76a5b2ba0f",
+    "url": "/assignment-3-cathgiese/static/css/main.c4cf8c21.chunk.css"
+  },
+  {
+    "revision": "0b688198d141b9e53c98",
+    "url": "/assignment-3-cathgiese/static/js/2.12cc552b.chunk.js"
+  },
+  {
+    "revision": "f3856b2f01fce324a8716c42650f3d79",
+    "url": "/assignment-3-cathgiese/static/js/2.12cc552b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bfcb6979b76a5b2ba0f",
+    "url": "/assignment-3-cathgiese/static/js/main.86933e7f.chunk.js"
+  },
+  {
+    "revision": "6d1e65ce7a08456da3ea",
+    "url": "/assignment-3-cathgiese/static/js/runtime-main.c9c1e10d.js"
+  }
+]);
